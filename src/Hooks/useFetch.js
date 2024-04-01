1c10d986004ed07ca0c3ca309cfe15fadbbd69dc
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2024-04-01 22:09:54 +0600

    Update useFetch.js

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -9,7 +9,7 @@ function useFetch() {
     async function fetchData(url) {
         isLoading.value = true
         try {
-            const data = await axios.get(url, { baseURL: 'https://fyrre-newseller-spa-cntxj1skf-wxzvrds-projects.vercel.app/' })
+            const data = await axios.get(url)
             responseData.value = await data.data
         } catch (e) {
             error.value = e
